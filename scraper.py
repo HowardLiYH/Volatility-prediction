8d6f44ecfeeeb6405a8dfd292e9f2584a16fe31b
Yuhao Li <yuhaoli0204@gmail.com>
2023-03-31 20:42:15 +0800

    Update on Mar31

---
+++ scraper.py
@@ -0,0 +1,47 @@
+import requests
+import pandas as pd
+import numpy as np
+from time import gmtime, strftime
+import time
+from google.cloud import bigquery
+import os
+from csv import writer
+
+
+# PROJECT = os.environ.get('GCP_PROJECT')
+# DATASET = os.environ.get('BQ_DATASET')
+# CRAWL_INTERVAL = int(os.environ.get('CRAWL_INTERVAL'))
+# TABLE = 'all'
+
+# table = f"{PROJECT}.{DATASET}.{TABLE}"
+
+
+def main():
+    while True:
+        r = requests.get("https://api.binance.com/api/v3/depth",
+                     params=dict(symbol="BTCBUSD"))
+        if r.status_code == 200:
+            result = r.json()
+            new_row = process(result)
+
+            with open('new_data_331.csv', 'a') as f_object:
+                writer_object = writer(f_object)
+                writer_object.writerow(new_row)
+
+        else:
+            print(r.status_code)
+        time.sleep(4)
+
+
+def process(result):
+    scrape_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    lastUpdateId = result['lastUpdateId']
+
+    bids = np.array(result['bids'][:20])
+    asks = np.array(result['asks'][:20])
+    combine = np.concatenate((bids,asks)).reshape((80,))
+
+    final = np.concatenate((np.array([scrape_time, lastUpdateId]),combine))
+    return final
+
+main()
